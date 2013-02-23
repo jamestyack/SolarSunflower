28b00f732c82a71e944198f876041029b9b40189
Kevin Clough <kevinclough@gmail.com>
2013-02-23 13:39:52 -0500

    Added rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   authenticated :user do
     root :to => 'home#index'
   end
