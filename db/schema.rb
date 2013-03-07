54980d0de5fba902bcb4b616e459eb6a4837d2e3
jasonblanchard <jas.blanchard@gmail.com>
2013-03-07 13:13:13 -0500

    Added collection_time and real timestamp for examplePOST

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130307180327) do
+ActiveRecord::Schema.define(:version => 20130307180959) do
 
   create_table "collection_points", :force => true do |t|
     t.text     "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130307180327) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.integer  "collection_point_id"
+    t.datetime "collection_time"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
