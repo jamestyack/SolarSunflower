89903b5a20390342cfa0586ee475b59525d83bc8
jasonblanchard <jas.blanchard@gmail.com>
2013-03-07 12:48:29 -0500

    Moved soil moisture save logic into the datacollection controller

--- app/controllers/data_collection_controller.rb
+++ app/controllers/data_collection_controller.rb
@@ -2,10 +2,12 @@ class DataCollectionController < ApplicationController
 
     def submit
         if params[:type] == 'soilmoisture'
-            redirect_to create_soilmoisture_path(:data => params[:data])
+            Soilmoisture.create!(params[:data])
         end
 
         respond_to do |format|
             format.json { head :ok }
         end
     end
+
+end
--- app/controllers/soilmoisture_controller.rb
+++ app/controllers/soilmoisture_controller.rb
@@ -1,6 +1,2 @@
 class SoilmoistureController < ApplicationController
-    def create
-        @soilmoisture = Soilmoisture.new(params[:data])
-        @soilmoisture.save!
-    end
 end
