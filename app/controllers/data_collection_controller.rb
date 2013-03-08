af0060fd3ad841e97febf1406dffe5f6b9f3c3a6
jasonblanchard <jas.blanchard@gmail.com>
2013-03-07 19:33:26 -0500

    Trying to ignore csrf temporarily

--- app/controllers/data_collection_controller.rb
+++ app/controllers/data_collection_controller.rb
@@ -1,5 +1,7 @@
 class DataCollectionController < ApplicationController
 
+    skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+
     def submit
         if params[:type] == 'soilmoisture'
             Soilmoisture.create!(params[:data])
