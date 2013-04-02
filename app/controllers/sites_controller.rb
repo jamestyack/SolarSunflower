c970ca2caefa853369ff577e1f0c49d607d919c2
jasonblanchard <jas.blanchard@gmail.com>
2013-04-02 19:42:04 -0400

    Added json response for soil points at a location

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,21 @@
 class SitesController < ApplicationController
     def show
         @site = Site.find(params[:id])
+        @collection_points = @site.collection_points
+        @soilmoistures = {}
+
+        @collection_points.each do |point|
+            @soilmoistures[point.name] = []
+
+            point.soilmoistures.each do |soil|
+                @soilmoistures[point.name] << soil
+            end
+        end
+
+
+        respond_to do |format|
+            format.html
+            format.json { render :json => @soilmoistures.to_json }
+        end
     end
 end
