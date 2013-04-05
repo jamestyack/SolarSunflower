025990aed0016917c9af9f421d5e4d9ab8db7dfb
jasonblanchard <jas.blanchard@gmail.com>
2013-04-05 08:44:18 -0400

    Detting data for each depth

--- app/assets/javascripts/graph_soil_moisture.js
+++ app/assets/javascripts/graph_soil_moisture.js
@@ -1,4 +1,7 @@
 var graph_data = [];
+var deptha_data = [];
+var depthb_data = [];
+var depthc_data = [];
 
 $.ajax({
     type: 'GET',
@@ -14,4 +17,23 @@ function setData(data) {
         graph_data.push(data[i].soilMoisture);
     }
     console.log(graph_data);
+
+    parse_depth_data(graph_data, deptha_data, "deptha");
+    parse_depth_data(graph_data, depthb_data, "depthb");
+    parse_depth_data(graph_data, depthc_data, "depthc");
+
+    console.log(deptha_data);
+    console.log(depthb_data);
+    console.log(depthc_data);
 }
+
+function parse_depth_data(allData, depth_data, set_depth) {
+    for (i=0; i < allData.length; i++) {
+
+        for (c=0; c < allData[i].length; c++) {
+            depth_data.push(allData[i][c][set_depth]);
+        }
+
+    }
+}
+
