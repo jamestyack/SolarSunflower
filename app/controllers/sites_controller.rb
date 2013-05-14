031b6dc6f5197f77afe5ef4a81f0881f1615a678
jasonblanchard <jas.blanchard@gmail.com>
2013-05-14 19:43:51 -0400

    Re-added endpoint stuff

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -17,8 +17,19 @@ class SitesController < ApplicationController
         @all_points << @soilmoisture_points_b
         @all_points << @soilmoisture_points_c
 
+        @collection_points.each_with_index do |point, key|
+            @soilmoistures[key] = Hash.new
+            @soilmoistures[key]['pointName'] = point.name
+            @soilmoistures[key]['soilMoistureData'] = []
+
+            point.soilmoistures.each do |soil|
+                @soilmoistures[key]['soilMoistureData'] << soil
+            end
+        end
+
         respond_to do |format|
             format.html
+            format.json { render :json => @soilmoistures.to_json }
         end
     end
 end
